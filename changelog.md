3afcad8e6a89b0e61e3d48e83c024b3fc7d1a251
valeartola <mv.artola@alumno.um.edu.ar>
2024-08-22 21:17:08 -0300

    try to fix position in board

--- changelog.md
+++ changelog.md
@@ -5,7 +5,11 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
-## [0.0.7] - 2024-08-20
+## [0.0.8] - 2024-08-22
+### Fixxed
+-Arrangement of __init__ function in board.py
+
+## [0.0.7] - 2024-08-21
 ### Added
 -Added __str__ to board.py 
 
--- juego/board.py
+++ juego/board.py
@@ -8,49 +8,21 @@ from juego.peon import Peon
 
 class Board():
     def __init__(self):
-        self.__positions__ = []
-        for _ in range(8):
-            col = []
-            for _ in range(8):
-                col.append(None)
-            self.__positions__.append(col)
-        self.__positions__[0][0] = Torre("BLANCO") 
-        self.__positions__[0][1] = Caballo("BLANCO")
-        self.__positions__[0][2] = Alfil("BLANCO")
-        self.__positions__[0][3] = Reina("BLANCO")
-        self.__positions__[0][4] = Rey("BLANCO")
-        self.__positions__[0][5] = Alfil("BLANCO")          
-        self.__positions__[0][6] = Caballo("BLANCO")      
-        self.__positions__[0][7] = Torre("BLANCO")
-        self.__positions__[1][0] = Peon("BLANCO")
-        self.__positions__[1][1] = Peon("BLANCO")
-        self.__positions__[1][2] = Peon("BLANCO")
-        self.__positions__[1][3] = Peon("BLANCO")
-        self.__positions__[1][4] = Peon("BLANCO")
-        self.__positions__[1][5] = Peon("BLANCO")
-        self.__positions__[1][6] = Peon("BLANCO")
-        self.__positions__[1][7] = Peon("BLANCO")
-        ###                 
-        self.__positions__[7][0] = Torre("NEGRO")
-        self.__positions__[7][1] = Caballo("NEGRO")
-        self.__positions__[7][2] = Alfil("NEGRO")
-        self.__positions__[7][3] = Reina("NEGRO")                
-        self.__positions__[7][4] = Rey("NEGRO")        
-        self.__positions__[7][5] = Alfil("NEGRO")
-        self.__positions__[7][6] = Caballo("NEGRO")
-        self.__positions__[7][7] = Torre("NEGRO")                
-        self.__positions__[6][0] = Peon("NEGRO")        
-        self.__positions__[6][1] = Peon("NEGRO")
-        self.__positions__[6][2] = Peon("NEGRO")
-        self.__positions__[6][3] = Peon("NEGRO")
-        self.__positions__[6][4] = Peon("NEGRO")
-        self.__positions__[6][5] = Peon("NEGRO")
-        self.__positions__[6][6] = Peon("NEGRO")
-        self.__positions__[6][7] = Peon("NEGRO")
- 
+        self.__positions__ = [[None for _ in range(8)] for _ in range(8)]
+        
+        piezas_blancas = [Torre, Caballo, Alfil, Reina, Rey, Alfil, Caballo, Torre]
+        piezas_negras = [Torre, Caballo, Alfil, Reina, Rey, Alfil, Caballo, Torre]
+        
+        for i, pieza in enumerate(piezas_blancas):
+            self.__positions__[0][i] = pieza("BLANCO")
+            self.__positions__[1][i] = Peon("BLANCO")
+        
+        for i, pieza in enumerate(piezas_negras):
+            self.__positions__[7][i] = pieza("NEGRO")
+            self.__positions__[6][i] = Peon("NEGRO")
     
     def __str__(self):
-        board_str = ""
+        filas = ""
         for row in self.__positions__:
             for cell in row:
                 if cell is not None:
@@ -69,3 +41,4 @@ class Board():
     def is_valid_coordinate(self, row, col):
         # Verificar que las coordenadas estén en el rango válido (0 a 7)
         return 0 <= row < 8 and 0 <= col < 8
+
