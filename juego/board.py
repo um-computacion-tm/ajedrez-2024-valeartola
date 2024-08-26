7a35e349f9c9a08c5ed6db0c6c4dc28f8074a478
valeartola <mv.artola@alumno.um.edu.ar>
2024-08-26 11:22:25 -0300

    fix board

--- juego/board.py
+++ juego/board.py
@@ -8,29 +8,43 @@ from juego.peon import Peon
 
 class Board():
     def __init__(self):
-        self.__positions__ = [[None for _ in range(8)] for _ in range(8)]
-        
-        piezas_blancas = [Torre, Caballo, Alfil, Reina, Rey, Alfil, Caballo, Torre]
-        piezas_negras = [Torre, Caballo, Alfil, Reina, Rey, Alfil, Caballo, Torre]
-        
-        for i, pieza in enumerate(piezas_blancas):
-            self.__positions__[0][i] = pieza("BLANCO")
-            self.__positions__[1][i] = Peon("BLANCO")
-        
-        for i, pieza in enumerate(piezas_negras):
-            self.__positions__[7][i] = pieza("NEGRO")
-            self.__positions__[6][i] = Peon("NEGRO")
+        self.__positions__ = []
+        for _ in range(8):
+            col = []
+            for _ in range(8):
+                col.append(None)
+            self.__positions__.append(col)
+        self.__positions__[0][0] = Torre("BLANCO") 
+        self.__positions__[0][7] = Torre("BLANCO")         
+        self.__positions__[7][7] = Torre("NEGRO")      
+        self.__positions__[7][0] = Torre("NEGRO")  
+        self.__positions__[0][1] = Caballo("BLANCO")
+        self.__positions__[0][6] = Caballo("BLANCO")        
+        self.__positions__[7][6] = Caballo("NEGRO")     
+        self.__positions__[7][1] = Caballo("NEGRO")   
+        self.__positions__[0][2] = Alfil("BLANCO")
+        self.__positions__[0][5] = Alfil("BLANCO")
+        self.__positions__[7][5] = Alfil("NEGRO") 
+        self.__positions__[7][2] = Alfil("NEGRO")
+        self.__positions__[0][3] = Rey("BLANCO")
+        self.__positions__[7][3] = Rey("NEGRO")
+        self.__positions__[0][4] = Reina("BLANCO")
+        self.__positions__[7][4] = Reina("NEGRO")
+
+        for col in range(8):
+            self.__positions__[1][col] = Peon("BLANCO")
+            self.__positions__[6][col] = Peon("NEGRO")           
     
     def __str__(self):
-        filas = ""
+        board_str = ""
         for row in self.__positions__:
             for cell in row:
                 if cell is not None:
                     board_str += str(cell)
                 else:
                     board_str += " "
-                board_str += "\n"
-            return board_str
+            board_str += "\n"
+        return board_str
     
     def get_piece(self, row, col):
         # Retorna la pieza en la coordenada especificada
