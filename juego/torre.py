0be19f1ee3192326f3bb955188d540d5f111895e
valeartola <mv.artola@alumno.um.edu.ar>
2024-09-02 10:30:31 -0300

    Moves rook

--- juego/torre.py
+++ juego/torre.py
@@ -4,9 +4,23 @@ class Torre(Piece):
     white_str  = "♖"
     black_str  = "♜"
 
+
+    def valid_positions(self, from_row, from_col, to_row, to_col):
+        possible_positions = (
+            self.possible_positions(from_row, from_col, direction="vertical") +
+            self.possible_positions(from_row, from_col, direction="horizontal")
+        )
+        return (to_row, to_col) in possible_positions
+
     def possible_positions_vd(self, row, col):
         possibles = []
-        for next_row in range(row +1, 8): #range(inicio, fin que no esta incluido)
+        for next_row in range(row + 1, 8):
+            # que la celda que sigue no este ocupada..
+            other_piece = self.__board__.get_piece(next_row, col)
+            if other_piece is not None:
+                if other_piece.__color__ != self.__color__:
+                    possibles.append((next_row, col))
+                break
             possibles.append((next_row, col))
         return possibles
     
@@ -16,4 +30,26 @@ class Torre(Piece):
             possibles.append((next_row, col))
         return possibles    
     
+    def possible_positions_hd(self, row, col):
+        possibles = []
+        for next_col in range(col + 1,8):            
+            other_piece = self.__board__.get_piece(row, next_col)
+            if other_piece is not None:
+                if other_piece.__color__ != self.__color__:
+                    possibles.append((row, next_col))
+                break  
+            possibles.append((row, next_col))
+        return possibles                          
+
+    def possible_positions_hi(self, row, col):
+        possibles = []
+        for next_col in range(col - 1,-1,-1):            
+            other_piece = self.__board__.get_piece(row, next_col)
+            if other_piece is not None:
+                if other_piece.__color__ != self.__color__:
+                    possibles.append((row, next_col))
+                break  
+            possibles.append((row, next_col))
+        return possibles                          
+
 
