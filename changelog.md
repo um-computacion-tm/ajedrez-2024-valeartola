0903cea7446274b3e15a9e4ea083fb59ca386684
valeartola <mv.artola@alumno.um.edu.ar>
2024-09-15 12:45:03 -0300

    Added peon

--- changelog.md
+++ changelog.md
@@ -5,6 +5,10 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [0.0.22] - 2024-09-15
+### Added
+-Added somes moves to peon.py
+
 ## [0.0.21] - 2024-09-13
 ### Added
 -Added somes moves to reina.py
--- juego/peon.py
+++ juego/peon.py
@@ -5,9 +5,50 @@ class Peon(Piece):
     white_str  = "♙"
     black_str  = "♟"
 
-    # def __str__(self):
-    #     if self.__color__ == "BLANCO":
-    #         return "♙"
-    #     else:
-    #         return "♟"
-            
+    def get_possible_positions(self, from_row, from_col):
+        possibles = self.get_possible_positions_move(
+            from_row,
+            from_col,
+        )
+        possibles.extend(
+            self.get_possible_positions_eat(from_row, from_col)
+        )
+        return possibles
+
+    def get_possible_positions_eat(self, from_row, from_col):
+        if self.__color__ == "BLACK":
+            other_piece = self.__board__.get_piece(from_row + 1, from_col + 1)
+            if other_piece and other_piece.__color__ == "WHITE":
+                return [(from_row + 1, from_col + 1)]
+
+        return []
+
+    def get_possible_positions_move(self, from_row, from_col):
+        if self.__color__ == "BLACK":
+            if self.__board__.get_piece(from_row + 1, from_col) is None:
+                if (
+                    from_row == 1 and
+                    self.__board__.get_piece(from_row + 2, from_col) is None
+                ):
+                    return [
+                        (from_row + 1, from_col),
+                        (from_row + 2, from_col)
+                    ]
+                else:
+                    return [
+                        (from_row + 1, from_col),
+                    ]
+        else:
+            if from_row == 6:
+                return [
+                    (from_row - 1, from_col),
+                    (from_row - 2, from_col)
+                ]
+            else:
+                if self.__board__.get_piece(from_row - 1, from_col) is None:
+                    return [
+                        (from_row - 1, from_col),
+                    ]
+                else:
+                    return []
+        return []
