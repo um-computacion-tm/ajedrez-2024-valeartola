d303aac4a5019679efb72b63fa55c82fc44f6fe2
valeartola <mv.artola@alumno.um.edu.ar>
2024-09-25 20:26:15 -0300

    add new king

--- juego/reina.py
+++ juego/reina.py
@@ -22,3 +22,13 @@ class Reina(Piece):
         for next_row in range (row - 1, -1, -1):
             possibles.append((next_row,col))
         return possibles
+
+
+"""    def get_possible_positions(self, from_row, from_col):
+        return self.possible_orthogonal_positions(
+            from_row,
+            from_col,
+        ) + self.possible_diagonal_positions(
+            from_row,
+            from_col,
+        )"""
--- juego/rey.py
+++ juego/rey.py
@@ -5,8 +5,20 @@ class Rey(Piece):
     white_str  = "♔"
     black_str  = "♚"
     
-    # def __str__(self):
-    #     if self.__color__ == "BLANCO":
-    #         return "♔"
-    #     else:
-    #         return "♚"
+
+    def get_possible_positions(self, from_row, from_col):
+        possibles = self.possible_orthogonal_positions(
+            from_row,
+            from_col,
+        ) + self.possible_diagonal_positions(
+            from_row,
+            from_col,
+        )
+        possible_king = []
+        for (possible_row, possible_col) in possibles:
+            if (
+                abs(from_row - possible_row) <= 1 and
+                abs(from_col - possible_col) <= 1
+            ):
+                possible_king.append((possible_row, possible_col))
+        return possible_king
