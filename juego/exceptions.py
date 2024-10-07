32ea827a11dc6810e53d6d26f22534535cac1f1f
valeartola <mv.artola@alumno.um.edu.ar>
2024-10-06 23:01:26 -0300

    add new exceptions

--- juego/exceptions.py
+++ juego/exceptions.py
@@ -2,12 +2,27 @@ class InvalidMove(Exception):
     message = "Movimieto de pieza invalido"
     def __str__(self):
         return self.message
+    
+class InvalidCoordinate(Exception):
+    message = "Coordenada invalida"
 
 class InvalidTurn(InvalidMove):
     message = "No puedes mover pieza de otro jugador"
 
 class EmptyPosition(InvalidMove):
-    message = "La posicion esta vacia"
+    message = "La posición esta vacia"
 
 class OutOfBoard(InvalidMove):
-    message = "La posicion indicada se encuentra fuera del tablero"
+    message = "La posición indicada se encuentra fuera del tablero"
+
+class InvalidMoveSameColor(InvalidMove):
+    message = "No puedes capturar una pieza de tu mismo color"
+
+class InvalidSamePlace(InvalidMove):
+    message = "No puede mover una pieza en su misma posición "
+
+class OutOfBoard(InvalidMove):
+    message = "La posición indicada se encuentra fuera del tablero"
+
+class Withoutcoordinate(InvalidMove):
+    message = "Coordenada vacia"
