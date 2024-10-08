d5afebf3de78e0db560775c76579983a9e1b0e7d
valeartola <mv.artola@alumno.um.edu.ar>
2024-10-08 09:27:38 -0300

    add new main

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from juego.chess import Chess
 from juego.exceptions import InvalidMove
 from juego.exceptions import Withoutcoordinate
+from juego.exceptions import NoneEmpate
 
 def main():
     chess = Chess()
@@ -14,10 +15,23 @@ def play(chess):
        print("---------------AJEDREZ---------------")
        print(chess.show_board())
        print("turno: ", chess.turn)
-       offer_draw = input("¿Deseas ofrecer un empate? (si/no): ").strip().lower()
-       if offer_draw == 'si':
-            chess.offer_draw()  
-            return False
+       offer_draw = ""
+       while (offer_draw not in ("si", "no")):
+            offer_draw = input("¿Deseas ofrecer un empate? (si/no): ").strip().lower()
+            if offer_draw == 'si':
+                    chess.change_turn()
+                    print(f"turno: {chess.__turn__}")
+                    accept_draw = ""
+                    while (accept_draw not in ("si", "no")):
+                        accept_draw = input("¿Acepta empate? (si/no): ").strip().lower()
+                    if accept_draw == "si":
+                        print("Ambos jugadores han acordado un empate.")
+                        print("---------------------")  
+                        chess.__game_over__ = True
+                        chess.__winner__ = "Ninguno"
+                        return
+
+       
        from_row = int(input("Desde la fila:  "))
        from_col = int(input("Desde la columna:  "))
        to_row = int(input("A la fila:  "))
