eee2dde01bcaa0e093ffa321e25625409029f978
valeartola <mv.artola@alumno.um.edu.ar>
2024-10-12 11:50:41 -0300

    refactoring

--- juego/alfil.py
+++ juego/alfil.py
@@ -6,5 +6,10 @@ class Alfil(Piece):
     black_str  = "♝"
 
     def valid_move(self, from_row, from_col, to_row, to_col):
-        return self._is_diagonal_move(from_row, from_col, to_row, to_col) and \
-               self._is_path_clear_diagonal(from_row, from_col, to_row, to_col)
+        if not self._is_diagonal_move(from_row, from_col, to_row, to_col):
+            return False
+        
+        if not self._is_path_clear_diagonal(from_row, from_col, to_row, to_col):
+            return False
+        
+        return True
